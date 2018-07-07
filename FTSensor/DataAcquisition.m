780465d3030d41d9c6d6134d5a7ed5c3f5e02304
Virginia <virginia@home2.local>
2018-07-07 12:29:57 +0200

    Added trial epochs

--- FTSensor/DataAcquisition.m
+++ FTSensor/DataAcquisition.m
@@ -1,7 +1,7 @@
 % DAQ from F/T Sensor
 function DataAcquisition()
 savefile = 1;
-filename = '20180705_FS_test.txt';
+filename = '20180706_FS_test.txt';
 filepath = ['D:\Student_experiments\Virginia\FTSensor\Data\',filename];
 
 device = daq.getDevices;
@@ -33,12 +33,12 @@ if ~isempty(device)
     hp = plot(0,0,'g','LineWidth',2);
     hold on;
     %plot(targetPosx,targetPosy,'r*')
-    for j = 1:length(targetPosx)
-        targetCir = circle(rCirTarget,targetPosx(j),targetPosy(j));
-        plot(targetCir(:,1),targetCir(:,2),'r','LineWidth',2)
-        %rectangle('Position',[targetPosx(j),targetPosy(j),rCirTarget,rCirTarget],...
-        %    'Curvature',[1,1],'FaceColor','r','EdgeColor','r');
-    end
+%     for j = 1:length(targetPosx)
+%         targetCir = circle(rCirTarget,targetPosx(j),targetPosy(j));
+%         plot(targetCir(:,1),targetCir(:,2),'r','LineWidth',2)
+%         %rectangle('Position',[targetPosx(j),targetPosy(j),rCirTarget,rCirTarget],...
+%         %    'Curvature',[1,1],'FaceColor','r','EdgeColor','r');
+%     end
     title('2D Force');
     xlabel('F_x [N]'); ylabel('F_y [N]');
     set(gca,'xlim',targetForce*[-1.2 1.2],'ylim',targetForce*[-1.2 1.2]);
@@ -48,12 +48,19 @@ if ~isempty(device)
     % Set file if saving option is enabled
     if savefile
         fid = fopen(filepath,'wt');
-        fprintf(fid, ' TimeStamp\t Fx [N]\t Fy [N]\n');
+        fprintf(fid, 'Trialnum\t State\t TimeStamp\t Fx [N]\t Fy [N]\n');
     end
     
     % Start acquiring data
-    targetAngle = -1;
-    h = addlistener(s,'DataAvailable',@(src,event) processData(event,offset,targetAngle,hp));
+    movemtime = 5;
+    holdtime = 0.5;
+    trialnum = 0;
+    cursorholdout = 0;
+    countstate = 0;
+    state = 'start';
+    origstate = 'start';
+    global tstart tmovem trelax targetCir tholdstart ht htxt
+    h = addlistener(s,'DataAvailable',@(src,event) processData(event,offset,hp));
     s.IsContinuous = true;
     s.Rate = 1000; % scans/sec, samples/sec?
     %s.NotifyWhenDataAvailableExceeds = 100; % Call listener when x samples are available
@@ -81,7 +88,7 @@ else
 end
 
 %% Event handles
-    function[cursorCir] = processData(event,offset,targetAngle,hp)
+    function processData(event,offset,hp)
         calibMat = [-0.56571    -0.01516    1.69417     -31.81016   -0.35339    33.73195;...
                 -0.67022    37.27575    1.14848     -18.75980   0.34816     -19.33789;...
                 19.06483    0.45530     18.57588    0.72627     19.51260    0.65624;...
@@ -107,22 +114,65 @@ end
         forceDatax = mean(forceData(:,1));
         forceDatay = mean(forceData(:,2));
         cursorCir = circle(rCirCursor,forceDatax,forceDatay);
-        set(hp,'xdata',[cursorCir(:,1)]','ydata',[cursorCir(:,2)']);
+        set(hp,'xdata',cursorCir(:,1)','ydata',cursorCir(:,2)');
+        
+        if strcmp(state,origstate)&&countstate==0
+            countstate = countstate+1;
+            fprintf('%d ',countstate)
+            htxt = text(6.5, 4, state, 'clipping', 'off','Fontsize',12);
+        elseif ~strcmp(state,origstate)&&countstate>0
+            countstate = 0;
+            delete(htxt)
+        end
+        
         %rectangle('Position',[forceDatax,forceDatay,rCirCursor,rCirCursor],...
         %    'Curvature',[1,1],'FaceColor','g');
         %fill([cursorCir(:,1)]',[cursorCir(:,2)'],'r')
 
         drawnow;
         
+       origstate = state;
+
         switch state
             case 'start'
+                trialnum = trialnum+1;
                 tstart = tic;
+                iAngle = randi(8);
+                targetCir = circle(rCirTarget,targetPosx(iAngle),targetPosy(iAngle));
+                ht = plot(targetCir(:,1),targetCir(:,2),'r','LineWidth',2);
                 state = 'movement';
             case 'movement'
-                if 
-        
+                if cursorInTarget(cursorCir,targetCir) && (toc(tstart)-tstart) <= movemtime
+                    tmovem = toc(tstart);
+                    state = 'hold';
+                    tholdstart = tic;
+                elseif (toc(tstart)-tstart)> movemtime
+                    state = 'fail';
+                end
+            case 'hold'
+                if ~cursorInTarget(cursorCir,targetCir) && toc(tholdstart)-tmovem <= holdtime
+                    cursorholdout = cursorholdout+1;
+                elseif cursorholdout > 10 && toc(tholdstart)-tmovem > holdtime
+                    state = 'fail';
+                    trelax = tic;
+                elseif cursorholdout <= 10 && toc(tholdstart)-tmovem > holdtime
+                    state = 'success';
+                    trelax = tic;
+                end
+            case 'fail'
+                state = 'relax';
+                delete(ht)                
+            case 'success'
+                state = 'relax';
+                delete(ht)
+            case 'relax'
+                if cursorInTarget(cursorCir,circle(rCirCursor+0.1,0,0)) && toc(trelax) > 1
+                    state = 'start';
+                end
+        end
+
         if savefile
-            fprintf(fid, '%1.4f\t %1.4f\t %1.4f\n',time(1),forceDatax,forceDatay);
+            fprintf(fid, '%d\t %s\t %1.4f\t %1.4f\t %1.4f\n',trialnum,state,time(1),forceDatax,forceDatay);
         end
     end
 end
--- FTSensor/cursorInTarget.m
+++ FTSensor/cursorInTarget.m
@@ -1,7 +1,14 @@
 function[cursorin] = cursorInTarget(cursorCir,targetCir)
-areaoverlap = polyarea(cursorCir(:,1)-targetCir(:,1),cursorCir(:,2)-targetCir(:,2));
-areatarget = polyarea(targetCir(:,1),targetCir(:,2));
-if areaoverlap/areatarget >= 0.9
+rcursor = sqrt((cursorCir(1,1)-cursorCir(round(length(cursorCir)/2),1))^2+(cursorCir(1,2)-cursorCir(round(length(cursorCir)/2),2))^2)/2;
+rtarget = sqrt((targetCir(1,1)-targetCir(round(length(targetCir)/2),1))^2+(targetCir(1,2)-targetCir(round(length(targetCir)/2),2))^2)/2;
+
+ccursor = [cursorCir(1,1)-rcursor,cursorCir(1,2)];
+ctarget = [targetCir(1,1)-rtarget,targetCir(1,2)];
+
+
+d = sqrt((ccursor(1)-ctarget(1))^2+(ccursor(2)-ctarget(2))^2);
+
+if d < rcursor+rtarget
     cursorin = true;
 else
     cursorin = false;
